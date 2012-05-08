b3d3f1e4a79ba67b7a7ee0e336a50ed74d346bb4
Paul Nossoff <kaluzhanin@gmail.com>
2012-05-08 13:41:59 +0400

    Pacha parser

--- lib/club-parser.rb
+++ lib/club-parser.rb
@@ -4,5 +4,6 @@ require "club-parser/version"
 
 module ClubParser
   autoload :AmnesiaParser, File.expand_path('club-parser/amnesia.rb', File.dirname(__FILE__))
+  autoload :PachaParser, File.expand_path('club-parser/pacha.rb', File.dirname(__FILE__))
   autoload :Event, File.expand_path('club-parser/event.rb', File.dirname(__FILE__))
 end
